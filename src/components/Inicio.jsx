812b7d5ce7974ed64ff01bc582a6c7c1567fcd0e
Matthew <matt.rt30@gmail.com>
2022-04-12 20:09:38 -0400

    Comentarios, validaciones, optimizacion componentes, nuevo inicio y card de rutina y dieta parte 3

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -16,22 +16,28 @@ const Inicio = () => {
           <div className="carousel-item active">
             <img src="https://cdn.discordapp.com/attachments/960953221596196927/963479279776178227/Vic2.png" style={{ "height": "600px", "width": "100%" }} className="d-block" alt="..." />
             <div className="carousel-caption d-none d-md-block">
-              <h5>Vic2</h5>
-              <p>Aplicación de dietas y ejercicios</p>
+              <div className='bg-dark bg-opacity-50 card shadow text-light'>
+                <h5>Vic2</h5>
+                <p>Aplicación de dietas y ejercicios</p>
+              </div>
             </div>
           </div>
           <div className="carousel-item">
             <img src="https://static1.abc.es/media/bienestar/2021/09/23/mitos-deporte-kgvC--1200x630@abc.jpg" style={{ "height": "600px", "width": "100%" }} className="d-block" alt="..." />
             <div className="carousel-caption d-none d-md-block">
-              <h5>Rutinas de ejercicios</h5>
-              <p>Revisa y selecciona tus ejercicios favoritos para trabajar distintas secciones del cuerpo.</p>
+              <div className='bg-dark bg-opacity-50 card shadow text-light'>
+                <h5>Rutinas de ejercicios</h5>
+                <p>Revisa y selecciona tus ejercicios favoritos para trabajar distintas secciones del cuerpo.</p>
+              </div>
             </div>
           </div>
           <div className="carousel-item">
             <img src="https://st3.depositphotos.com/1027198/12984/i/450/depositphotos_129845962-stock-photo-diet-food-concept.jpg" style={{ "height": "600px", "width": "100%" }} className="d-block" alt="..." />
             <div className="carousel-caption d-none d-md-block">
-              <h5>Dietas</h5>
-              <p>Visualiza alimentos y dietas para adelgazar según distintos tipos de cuerpos o acompañar una rutina de ejercicios.</p>
+              <div className='bg-dark bg-opacity-50 card shadow text-light'>
+                <h5>Dietas</h5>
+                <p>Visualiza alimentos y dietas para adelgazar según distintos tipos de cuerpos o acompañar una rutina de ejercicios.</p>
+              </div>
             </div>
           </div>
         </div>
